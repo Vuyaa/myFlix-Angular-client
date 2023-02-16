9babe47899a0c5d5a0db40f3267b446cbfb90b97
Vuyaa <st_im@hotmail.com>
2023-02-16 02:00:11 +0100

    Added login and user registration form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { UserRegistrationFormComponent } from "./user-registration-form/user-registration-form.component";
+import { MatDialog } from "@angular/material/dialog";
+import { UserLoginFormComponent } from "./user-login-form/user-login-form.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'myFlix-Angular-client';
+  title = "myFlix-Angular-client";
+
+  constructor(public dialog: MatDialog) {}
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      width: "280px",
+    });
+  }
+
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: "280px",
+    });
+  }
 }
--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,11 +5,11 @@ import { catchError, map, Observable, throwError } from 'rxjs';
 
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://site--popkorny--w5mfxztkv9bc.code.run/';
+const apiUrl = 'https://site--popkorny--w5mfxztkv9bc.code.run';
 @Injectable({
   providedIn: 'root'
 })
-export class UserRegistrationService {
+export class FetchApiDataService {
   // Inject the HttpClient module to the constructor params
  // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {
@@ -17,7 +17,7 @@ export class UserRegistrationService {
  // Making the api call for the user registration endpoint
   public userRegistration(userDetails: any): Observable<any> {
     console.log(userDetails);
-    return this.http.post(apiUrl + 'users', userDetails).pipe(
+    return this.http.post(apiUrl + '/users', userDetails).pipe(
     catchError(this.handleError)
     );
   }
@@ -26,14 +26,14 @@ export class UserRegistrationService {
   userLogin(userDetails: any): Observable<any> {
     console.log(userDetails);
     return this.http
-      .post(apiUrl + "login", userDetails)
+      .post(apiUrl + "/login", userDetails)
       .pipe(catchError(this.handleError));
   }
 
   //self explanatory
   getMovies(): Observable<any> {
     const token = localStorage.getItem('token');
-    return this.http.get(apiUrl + 'movies', {headers: new HttpHeaders(
+    return this.http.get(apiUrl + '/movies', {headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
       })}).pipe(
@@ -44,7 +44,7 @@ export class UserRegistrationService {
   //self explanatory
   getMovie(): Observable<any> {
     const token = localStorage.getItem('token');
-    return this.http.get(apiUrl + 'movies/:title', {headers: new HttpHeaders(
+    return this.http.get(apiUrl + '/movies/:title', {headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
       })}).pipe(
@@ -55,7 +55,7 @@ export class UserRegistrationService {
   //self explanatory
   getDirector(): Observable<any> {
     const token = localStorage.getItem('token');
-    return this.http.get(apiUrl + 'movies/directors/:Name', {headers: new HttpHeaders(
+    return this.http.get(apiUrl + '/movies/directors/:Name', {headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
       })}).pipe(
@@ -66,7 +66,7 @@ export class UserRegistrationService {
   //self explanatory
   getGenre(): Observable<any> {
     const token = localStorage.getItem('token');
-    return this.http.get(apiUrl + 'movies/genres/:Name', {headers: new HttpHeaders(
+    return this.http.get(apiUrl + '/movies/genres/:Name', {headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
       })}).pipe(
@@ -77,7 +77,7 @@ export class UserRegistrationService {
   //self explanatory
   getUser(): Observable<any> {
     const token = localStorage.getItem('token');
-    return this.http.get(apiUrl + 'users/:Username', {headers: new HttpHeaders(
+    return this.http.get(apiUrl + '/users/:Username', {headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
       })}).pipe(
@@ -88,7 +88,7 @@ export class UserRegistrationService {
   //self explanatory
   getFavorite(): Observable<any> {
     const token = localStorage.getItem('token');
-    return this.http.get(apiUrl + 'users/:Username', {headers: new HttpHeaders(
+    return this.http.get(apiUrl + '/users/:Username', {headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
       })}).pipe(
@@ -111,7 +111,7 @@ export class UserRegistrationService {
   editUser(updatedDetails: any): Observable<any> {
     const token = localStorage.getItem('token');
     console.log(updatedDetails);
-    return this.http.put(apiUrl + 'users/:Username', updatedDetails,  {headers: new HttpHeaders(
+    return this.http.put(apiUrl + '/users/:Username', updatedDetails,  {headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
       })}).pipe(
@@ -121,7 +121,7 @@ export class UserRegistrationService {
   //self explanatory
   deleteUser(): Observable<any> {
     const token = localStorage.getItem('token');
-    return this.http.delete(apiUrl + 'users/:Username',  {headers: new HttpHeaders(
+    return this.http.delete(apiUrl + '/users/:Username',  {headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
       })}).pipe(
